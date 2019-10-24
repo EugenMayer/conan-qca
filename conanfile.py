486bc0b7b52f77c96b344a84587b134f4734c495
Sven Wehner <mail@svenwehner.de>
2019-10-24 17:27:58 +0000

    Use package() step and CMake's install properly. See: https://docs.conan.io/en/latest/howtos/cmake_install.html

--- conanfile.py
+++ conanfile.py
@@ -20,18 +20,20 @@ class QcaConan(ConanFile):
         "revision": "v2.2.1"
     }
 
-    def build(self):
-        cmake = CMake(self) # it will find the packages by using our auto-generated FindXXX.cmake files
+    def _configure_cmake(self):
+        cmake = CMake(self)
         cmake.definitions["BUILD_TESTS"] = "OFF"
-        cmake.definitions["CMAKE_INSTALL_PREFIX"] = "install"
         cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
         cmake.build()
-        cmake.install()
-        
+
     def package(self):
-        install_dir = "conan_install"
-        self.copy("*", src="install", keep_path=True, symlinks=True)
-    
+        cmake = self._configure_cmake()
+        cmake.install()
+
     def package_info(self):
         if self.settings.compiler == "Visual Studio":
             self.cpp_info.libs = ["qca-qt5.lib"]
