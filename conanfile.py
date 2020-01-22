9b71542b9829d5436c71c714af550ea511b3a36a
Sven Wehner <mail@svenwehner.de>
2020-01-22 14:17:50 +0100

    Build QCA with active USE_RELATIVE_PATHS option.

--- conanfile.py
+++ conanfile.py
@@ -23,6 +23,7 @@ class QcaConan(ConanFile):
     def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["BUILD_TESTS"] = "OFF"
+        cmake.definitions["USE_RELATIVE_PATHS"] = "ON"
         cmake.definitions["CMAKE_INSTALL_RPATH"] = '\\$ORIGIN:\\$ORIGIN/lib:\\$ORIGIN/../lib:.:lib:../lib' # Do not load other libraries, only local ones!
         cmake.definitions["CMAKE_EXE_LINKER_FLAGS"] = '-Wl,--unresolved-symbols=ignore-in-shared-libs' # Do not complain about missing transitive dependencies while linking!
         cmake.definitions["CMAKE_SHARED_LINKER_FLAGS"] = '-Wl,--unresolved-symbols=ignore-in-shared-libs' # Do not complain about missing transitive dependencies while linking!
