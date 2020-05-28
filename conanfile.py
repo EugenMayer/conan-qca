0cfec8a162640e9f827db06fb5b8f8601b6a8b59
Eugen Mayer <eugen.mayer@kontextwork.de>
2020-05-28 13:45:23 +0200

    Upgrade openssl to 1.1.1g to comply with qt 5.15.0

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ class QcaConan(ConanFile):
     generators = "cmake_find_package"
     requires = [
         "qt/5.15.0@kwconan/stable",
-        "openssl/1.1.1d"
+        "openssl/1.1.1g"
     ]
     scm = {
         "type": "git",
